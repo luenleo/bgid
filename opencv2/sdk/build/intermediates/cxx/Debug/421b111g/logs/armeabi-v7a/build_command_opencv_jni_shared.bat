95614dc743852d6c55ccfb0a3fd70617778fa7e9
liinx <1264508018@qq.com>
2024-01-12 14:59:23 +0800

    [修改] 修改项目结构
    - 删除opencv2文件夹

--- opencv2/sdk/build/intermediates/cxx/Debug/421b111g/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\SDK\\android-sdk-windows\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\AndroidProjects\\bgid\\opencv2\\sdk\\.cxx\\Debug\\421b111g\\armeabi-v7a" ^
-  opencv_jni_shared
